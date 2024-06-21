64b46abee8856a3032638f1050c263072eb528a4
«nastassiamak» <redfox.asya@gmail.com>
2024-06-21 15:45:31 +0300

    You_Can't_Code_Under_Pressure_#1.js

---
+++ JS/8kyu/You_Can't_Code_Under_Pressure_#1.js
@@ -0,0 +1,13 @@
+// DESCRIPTION: 
+// Code as fast as you can! You need to double the integer and return it.
+
+// SOLUTION
+function doubleInteger(i) {
+
+    return i * 2;
+}
+
+console.log(doubleInteger(6));
+console.log(doubleInteger(34));
+console.log(doubleInteger(22));
+
