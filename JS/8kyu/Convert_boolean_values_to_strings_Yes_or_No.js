d24530032d8433f6ece2eec9918e337fca06c014
«nastassiamak» <redfox.asya@gmail.com>
2024-07-06 14:24:57 +0300

    Convert_boolean_values_to_strings_Yes_or_No.js

---
+++ JS/8kyu/Convert_boolean_values_to_strings_Yes_or_No.js
@@ -0,0 +1,16 @@
+// DESCRIPTION:
+//
+//     Complete the method that takes a boolean value and return a "Yes" string for true,
+//     or a "No" string for false.
+
+//SOLUTION
+
+function boolToWord( bool ){
+    if (bool) {
+        return 'Yes';
+    } else {
+        return 'No';
+    }
+}
+
+console.log(boolToWord(true));
