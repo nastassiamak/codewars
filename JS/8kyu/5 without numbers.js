12b6873c2d9a0fd6a921100f61938a321d32acc1
«nastassiamak» <redfox.asya@gmail.com>
2024-09-04 14:32:43 +0300

    5 without numbers.js

---
+++ JS/8kyu/5 without numbers.js
@@ -0,0 +1,16 @@
+// DESCRIPTION:
+//
+//     Write a function that always returns 5
+//
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+//
+// Good luck :)
+
+
+//SOLUTION
+
+function unusualFive() {
+    return 'fivee'.length;
+}
+
+console.log(unusualFive());
