3edefb7ec47966fd489411477959ac23bdd93b4b
«nastassiamak» <redfox.asya@gmail.com>
2024-09-09 14:05:00 +0300

    Powers of 2.js

---
+++ JS/8kyu/Powers of 2.js
@@ -0,0 +1,23 @@
+// DESCRIPTION:
+//
+//     Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+//
+//     Examples
+//
+// n = 0  ==> [1]        # [2^0]
+// n = 1  ==> [1, 2]     # [2^0, 2^1]
+//n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+
+//SOLUTION
+
+function powersOfTwo(n){
+    let powers =[]
+    for (i=0; i<=n; i++){
+        let answer =2**i
+        powers.push(answer)
+    }
+    return powers;
+}
+
+console.log(powersOfTwo(4));
