60b4189f6bae0c6313c0f9b64ee88fd10427acf3
«nastassiamak» <redfox.asya@gmail.com>
2024-09-13 16:04:38 +0300

    Find the position!.js

---
+++ JS/8kyu/Find the position!.js
@@ -0,0 +1,15 @@
+// DESCRIPTION:
+//
+//     When provided with a letter, return its position in the alphabet.
+//
+// Input :: "a"
+//
+// Ouput :: "Position of alphabet: 1"
+
+
+//SOLUTION
+
+function position(letter) {
+    return `Position of alphabet: ${letter.charCodeAt() - 96}`
+}
+console.log(position(a));
