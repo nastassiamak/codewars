b0051a5ac3974b88a61fb5382d9cfc5b06ef0bb5
«nastassiamak» <redfox.asya@gmail.com>
2024-09-17 11:53:35 +0300

    Bin to Decimal.js

---
+++ JS/8kyu/Bin to Decimal.js
@@ -0,0 +1,11 @@
+// DESCRIPTION:
+//
+//     Complete the function which converts a binary number (given as a string) to a decimal number.
+
+//SOLUTION
+
+function binToDec(bin){
+    return parseInt(bin,2);
+}
+
+console.log(binToDec([1, '3', 7]))
