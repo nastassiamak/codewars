5e6979709402d4a6ecd245a81d225607fca31b3e
«nastassiamak» <redfox.asya@gmail.com>
2024-09-18 11:59:51 +0300

    Reversing Words in a String.js

---
+++ JS/8kyu/Reversing Words in a String.js
@@ -0,0 +1,19 @@
+// DESCRIPTION:
+//
+//     You need to write a function that reverses the words in a given string. Words are always separated by a single space.
+//
+//     As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
+//
+// Example (Input --> Output)
+//
+// "Hello World" --> "World Hello"
+// "Hi There." --> "There. Hi"
+// Happy coding!
+
+function reverse(string){
+    let str = string.split(' ');
+    return str.reverse().join(' ')
+
+}
+
+console.log(reverse("I am happy now."))
