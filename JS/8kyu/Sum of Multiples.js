c5509ea75226bf2e308d63fc7d6dc1a55916766a
«nastassiamak» <redfox.asya@gmail.com>
2024-09-25 11:55:00 +0300

    Sum of Multiples.js

---
+++ JS/8kyu/Sum of Multiples.js
@@ -0,0 +1,26 @@
+// DESCRIPTION:
+//
+//     Your Job
+//
+// Find the sum of all multiples of n below m
+//
+// Keep in Mind
+//
+// n and m are natural numbers (positive integers)
+// m is excluded from the multiples
+// Examples
+//
+// sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+// sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+// sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+// sumMul(4, -7)  ==> "INVALID"
+
+//SOLUTION
+function sumMul(n,m) {
+    let result = 0;
+    for(let i = 1; i * n < m; i++){
+        result += i * n
+    }
+    return result > 0 ? result : 'INVALID'
+}
+console.log(sumMul(2,9))
