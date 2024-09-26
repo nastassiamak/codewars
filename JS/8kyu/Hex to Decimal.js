d798bc56fc4ba1c2cfe4d77b869322b699d32170
«nastassiamak» <redfox.asya@gmail.com>
2024-09-26 11:29:59 +0300

    Hex to Decimal.js

---
+++ JS/8kyu/Hex to Decimal.js
@@ -0,0 +1,12 @@
+// DESCRIPTION:
+//
+//     Complete the function which converts hex number (given as a string) to a decimal number.
+
+//SOLUTION
+
+function hexToDec(hexString){
+    return  parseInt(hexString, 16);
+}
+
+console.log(hexToDec("-C"));
+console.log(hexToDec("d"));
