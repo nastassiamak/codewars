f75de32cf74743be9d52b335c8769dff8703f248
«nastassiamak» <redfox.asya@gmail.com>
2024-10-01 18:31:49 +0300

    Grasshopper - Function syntax debugging.js

---
+++ JS/8kyu/Grasshopper - Function syntax debugging.js
@@ -0,0 +1,13 @@
+// DESCRIPTION:
+//
+//     Grasshopper - Function syntax debugging
+//
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+
+//SOLUTION
+
+function main (verb, noun) {
+    return verb + noun
+}
+console.log(main("Hello", "World"));
