62cbfc2153dd942532fa36edb624fd35352ab34c
«nastassiamak» <redfox.asya@gmail.com>
2025-02-16 10:41:50 +0300

    Your order, please.js

---
+++ JS/6kyu/Your order, please.js
@@ -0,0 +1,26 @@
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+//
+//     Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+//
+// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+//
+//     Examples
+//
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
+
+function order(words){
+    if (!words) {
+        return '';
+    }
+
+    return words.split(' ')
+        .map(word => ({ word, position: word.match(/\d/)[0] })) // Создаем объект со словом и позицией
+        .sort((a, b) => a.position - b.position) // Сортируем по позиции
+        .map(item => item.word) // Извлекаем отсортированные слова
+        .join(' '); // Объединяем их в строку
+
+}
+
+console.log(order("is2 Thi1s T4est 3a")); //"Thi1s is2 3a T4est"
